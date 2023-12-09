db1b9830c6d2bfd1a1f45108c848d3682c6c8ec8
qkb2 <53836634+qkb2@users.noreply.github.com>
2023-12-09 02:49:55 +0100

    Add inversion transformer (reverses strings).

---
+++ src/main/java/pl/put/poznan/transformer/logic/InversionTransformer.java
@@ -0,0 +1,35 @@
+package pl.put.poznan.transformer.logic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InversionTransformer extends TransformationDecorator {
+
+    InversionTransformer(StringTransformer tr) {
+        super(tr);
+    }
+
+    public String transform() {
+        return inversionDecorate(super.transform());
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(InversionTransformer.class);
+
+
+    private String inversionDecorate(String s) {
+        logger.debug("inversion mode");
+        String result = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            char reverse_c = s.charAt(s.length()-1-i);
+            if (Character.isUpperCase(c)) {
+                reverse_c = Character.toUpperCase(reverse_c);
+            } else if (Character.isLowerCase(c)) {
+                reverse_c = Character.toLowerCase(reverse_c);
+            }
+            result += c;
+
+        }
+        return result;
+    }
+}
