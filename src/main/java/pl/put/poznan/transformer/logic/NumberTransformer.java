ee7908308a82fb156e2a011b8dea287a79d333ff
bartox7777 <bartosz.zelek@protonmail.com>
2023-12-12 22:05:56 +0100

    Implementation of NumberTransformer class

--- src/main/java/pl/put/poznan/transformer/logic/NumberTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/NumberTransformer.java
@@ -1,22 +1,20 @@
 package pl.put.poznan.transformer.logic;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NumberTransformer extends TransformationDecorator {
-    public static enum Mode {
-        ABBREVIATE,
-        EXPAND
-    };
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+public class NumberTransformer extends TransformationDecorator {
     NumberTransformer(StringTransformer tr) {
         super(tr);
-        this.mode = Mode.EXPAND;
-    }
-
-    NumberTransformer(StringTransformer tr, Mode mode) {
-        super(tr);
-        this.mode = mode;
     }
 
     @Override
@@ -26,10 +24,24 @@ public class NumberTransformer extends TransformationDecorator {
 
     private static final Logger logger = LoggerFactory.getLogger(NumberTransformer.class);
 
-    private Mode mode;
-
-    // dummy functionality (class needed for UML graph)
     private String numberDecorate(String s) {
+        logger.debug("number expand mode");
+        for (Map.Entry<String, String> entry : numberToText.entrySet()) {
+            s = s.replaceAll("\\b(?i)" + Pattern.quote(entry.getKey()) + "\\b", entry.getValue());
+        }
+
         return s;
     }
+
+    static {
+        try {
+            InputStream is = AbbreviationTransformer.class.getResourceAsStream("/numberToText.json");
+            numberToText = new ObjectMapper().readValue(is, new TypeReference<HashMap<String, String>>() {
+            });
+        } catch (IOException e) {
+            logger.error("Failed to load numberToText.json");
+        }
+    }
+
+    private static HashMap<String, String> numberToText;
 }
