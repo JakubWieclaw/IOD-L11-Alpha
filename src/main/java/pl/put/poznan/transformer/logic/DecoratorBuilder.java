b41acae92b1c0658703eae841909c94bbfc72053
qkb2 <53836634+qkb2@users.noreply.github.com>
2023-12-13 13:06:34 +0100

    Add documentation for DecoratorBuilder and InversionTransformer

--- src/main/java/pl/put/poznan/transformer/logic/DecoratorBuilder.java
+++ src/main/java/pl/put/poznan/transformer/logic/DecoratorBuilder.java
@@ -1,10 +1,26 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * This class builds a full string transformer by stacking decorators (StringTransformer)
+ * specified in String[] transforms.
+ */
 public class DecoratorBuilder {
 
-    // HOW to handle unknown transformer
-    // for now we ignore the value
+    /**
+     * This method goes through all the transforms in {@param transforms}.
+     * Every iteration it looks for transform name. When it finds a transform
+     * name like UPPER, LOWER, CAPITALIZE, ABBREVIATE, EXPAND, INVERSE, NUM_EXPAND
+     * this method stacks previous transformer into new one of the fitting type
+     * (e.g. if it finds UPPER it stacks prev. transformer into new LetterCaseTransformer)
+     * If transform name is not known it uses new ErrorTransformer
+     *
+     * @param baseTransformer base StringTransformer to be specified
+     * @param transforms list of commands corresponding to different transformers
+     * @return a built StringTransformer
+     */
     public static StringTransformer composeTransformer(StringTransformer baseTransformer, String[] transforms){
+        // HOW to handle unknown transformer
+        // for now we ignore the value
         StringTransformer base = baseTransformer;
         for (String transform : transforms){
             switch (transform){
--- src/main/java/pl/put/poznan/transformer/logic/InversionTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/InversionTransformer.java
@@ -3,19 +3,35 @@ package pl.put.poznan.transformer.logic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * A class that extends TransformationDecorator to provide functionality
+ * for inverting a string while also preserving letter case at every index.
+ */
 public class InversionTransformer extends TransformationDecorator {
 
     InversionTransformer(StringTransformer tr) {
         super(tr);
     }
 
+    /**
+     * Transforms the string by inverting it.
+     *
+     * @return the transformed string
+     */
     public String transform() {
         return inversionDecorate(super.transform());
     }
 
     private static final Logger logger = LoggerFactory.getLogger(InversionTransformer.class);
 
-
+    /**
+     * Decorates the string by inverting a string and changing the letter case
+     * of the letter at every index i in inverted string to be the same as letter case
+     * of the letter at that index in original string.
+     *
+     * @param s the string to be decorated
+     * @return the decorated string
+     */
     private String inversionDecorate(String s) {
         logger.debug("inversion mode");
         String result = "";
