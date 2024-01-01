ed8be1926435ffe5649dc6b23945392aab191d50
inf151860 <bartosz.zelek@student.put.poznan.pl>
2024-01-01 19:36:16 +0100

    Revert "fix of abbr mode"
    This reverts commit 0bdb9d7f6ff6ff13e7df1408e811cfccfb1a5133.

--- src/main/java/pl/put/poznan/transformer/logic/AbbreviationTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/AbbreviationTransformer.java
@@ -73,15 +73,13 @@ public class AbbreviationTransformer extends TransformationDecorator {
             case ABBREVIATE:
                 logger.debug("abbreviate mode");
                 for (Map.Entry<String, String> entry : abbrToFullForm.entrySet()) {
-                    s = s.replaceAll("\\b(?i)" + Pattern.quote(entry.getValue()) + "\\.?", entry.getKey());
+                    s = s.replaceAll("\\b(?i)" + Pattern.quote(entry.getValue()) + "\\b", entry.getKey());
                 }
                 return s;
             case EXPAND:
                 logger.debug("expand mode");
-
                 for (Map.Entry<String, String> entry : abbrToFullForm.entrySet()) {
-                    s = s.replaceAll("\\b(?i)" + Pattern.quote(entry.getKey()) + "\\.?", entry.getValue());
-
+                    s = s.replaceAll("\\b(?i)" + Pattern.quote(entry.getKey()) + "\\b", entry.getValue());
                 }
                 return s;
             default:
